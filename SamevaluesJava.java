98545b0e9c7d1adb591acb15132444f3e0508209
Альбина Фролова <afrolova@na.edu>
2017-02-07 22:22:05 -0600

    Added Same values project

---
+++ SamevaluesJava.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samevalues.java;
+import java.util.Scanner;
+/**
+ *
+ * @author albinafrolova
+ */
+public class SamevaluesJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1;
+        double num2, result1, result2;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter integer and double: ");
+        num1 = scan.nextInt();
+        num2 = scan.nextDouble();
+        
+        result1 = (double) num1/5;
+        num1 = num1%5;
+       
+        result2 = num2/5;
+        num2 = num2%5;
+        
+        System.out.println(result1 + " and " + result2);
+       
+        
+    }
+    
+}
