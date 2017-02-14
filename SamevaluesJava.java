f0806a75211bfa00c0d1d040a748c7fa58f80bf9
Albina Frolova <afrolova@na.edu>
2017-02-14 01:06:27 -0600

    Update SamevaluesJava.java

--- SamevaluesJava.java
+++ SamevaluesJava.java
@@ -5,10 +5,7 @@ import java.util.Scanner;
  * @author albinafrolova
  */
 public class SamevaluesJava {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         int num1;
         double num2, result1, result2;
