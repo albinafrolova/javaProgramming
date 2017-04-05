19fb7dd4c8a2be1c2db397e6cb778f18ad51f0b7
Альбина Фролова <afrolova@na.edu>
2017-04-05 08:54:06 -0500

    Added switch project

---
+++ Switch.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author albinafrolova
+ */
+public class Switch {
+
+    public static void main(String[] args) {
+       char m='a';
+       switch(m){
+           case 'a': System.out.print("1");
+           case 'b': System.out.print("2");
+           case 'c': System.out.print("3");           
+    }
+       System.out.println(" Done");
+    }
+    
+}
