dcd764874270828a4c71445928dcbe8d229ae3cd
Альбина Фролова <afrolova@na.edu>
2017-04-24 10:54:24 -0500

    Added Janitor class

---
+++ Hospital/Janitor.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * @author albinafrolova
+ */
+public class Janitor extends Administrator {
+
+    protected boolean sweeping;
+
+    public Janitor(String empName, int empNumber, String dept, boolean sw) {
+        super(empName, empNumber, dept);
+        sweeping = sw;
+    }
+
+    public void setIsSweeping(boolean isS) {
+        sweeping = isS;
+    }
+
+    public boolean getIsSweeping() {
+        return sweeping;
+    }
+
+    public String toString() {
+        return super.toString() + " by Sweeping: " + sweeping;
+    }
+
+    public void sweep() {
+        System.out.print(name + " is");
+        if (sweeping == false) {
+            System.out.print(" not");
+        }
+        System.out.println(" sweeping the floor.");
+    }
+}
