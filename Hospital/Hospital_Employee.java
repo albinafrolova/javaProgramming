03714ef2c973871dfaadab8033f4f845ebc1e836
Альбина Фролова <afrolova@na.edu>
2017-04-24 10:50:42 -0500

    Added Employee information class

---
+++ Hospital/Hospital_Employee.java
@@ -0,0 +1,38 @@
+/**
+ *
+ * @author albinafrolova
+ */
+public class Hospital_Employee {
+
+    protected String name;
+    protected int number;
+
+    public Hospital_Employee(String empName, int empNumber) {
+        name = empName;
+        number = empNumber;
+    }
+
+    public void setName(String empName) {
+        name = empName;
+    }
+
+    public void setNumber(int empNumber) {
+        number = empNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String toString() {
+        return name + " " + number;
+    }
+
+    public void work() {
+        System.out.println(name + " is the youngest employee in the hospital.");
+    }
+}
