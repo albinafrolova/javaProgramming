c14d3cc9427100a51f0bf757d7e0cc5b45f68cb7
Альбина Фролова <afrolova@na.edu>
2017-04-24 10:53:55 -0500

    Added Nurse class

---
+++ Hospital/Nurse.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author albinafrolova
+ */
+public class Nurse extends Hospital_Employee {
+
+    protected int numPatients;
+
+    public Nurse(String empName, int empNumber, int numPat) {
+        super(empName, empNumber);
+        numPatients = numPat;
+    }
+
+    public void setNumPatients(int pat) {
+        numPatients = pat;
+    }
+
+    public int getNumPatients() {
+        return numPatients;
+    }
+
+    public String toString() {
+        return super.toString() + " has " + numPatients + " patients this week.";
+    }
+
+    public void assist() {
+        System.out.println(name + " is a nurse with " + numPatients + " patients.");
+    }
+}
