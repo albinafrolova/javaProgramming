e1ff1dc90baa7f64c7b2c7880c1825bd39faae5e
Альбина Фролова <afrolova@na.edu>
2017-04-24 10:51:49 -0500

    Added Doctor's class

---
+++ Hospital/Doctor.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author albinafrolova
+ */
+public class Doctor extends Hospital_Employee {
+
+    protected String speciality;
+
+    public Doctor(String empName, int empNumber, String special) {
+        super(empName, empNumber);
+        speciality = special;
+    }
+
+    public void setSpecialty(String special) {
+        speciality = special;
+    }
+
+    public String getSpecialty() {
+        return speciality;
+    }
+
+    public String toString() {
+        return super.toString() + " " + speciality;
+    }
+
+    public void diagnose() {
+        System.out.println(name + " is a " + speciality + " doctor.");
+    }
+}
