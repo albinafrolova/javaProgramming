643e4c48ac26192f35264b638adda5a2b1e7c2cc
Альбина Фролова <afrolova@na.edu>
2017-04-24 10:52:52 -0500

    Added Surgeon class

---
+++ Hospital/Surgeon.java
@@ -0,0 +1,35 @@
+
+/**
+ *
+ * @author albinafrolova
+ */
+public class Surgeon extends Doctor {
+
+    protected boolean operating;
+
+    public Surgeon(String empName,int empNumber, String special, boolean isOper) {
+        super(empName, empNumber, special);
+        operating = isOper;
+    }
+
+    public void setIsOperating(boolean isOper) {
+        operating = isOper;
+    }
+
+    public boolean getIsOperating() {
+        return operating;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " operating: " + operating;
+    }
+
+    public void operate() {
+        System.out.print(name + " is");
+        if (operating == false) {
+            System.out.print(" not");
+        }
+        System.out.println("operating right now.");
+    }
+}
