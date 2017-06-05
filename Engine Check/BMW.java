dca1a6ee939c823b6dcfa34bc25c77f1c211214a
Альбина Фролова <afrolova@na.edu>
2017-06-05 12:11:45 +0600

    Added bmw file

---
+++ Engine Check/BMW.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exam;
+
+/**
+ *
+ * @author albinafrolova
+ */
+public class BMW extends Car{
+    public void engine(){
+        System.out.println("Start building a bmw car engine...");
+    }
+    public void drive(){
+        System.out.println("Start driving...");
+    }
+}
