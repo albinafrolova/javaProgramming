d130ccbe6dc295a7e34b83f3e7e93ffd1d0e7351
Альбина Фролова <afrolova@na.edu>
2017-06-21 23:43:26 +0600

    Added Electroinic class

---
+++ Retail/Electronic.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author albinafrolova
+ */
+public class Electronic {
+    public void  a() {
+        System.out.println("Electronic a");
+    }
+    public void b(){
+        System.out.println("Electronic b");
+    }
+    public void c(){
+        System.out.println("Electronic c");
+    }
+}
