767bf2624f5415c02f003917f248d5752cb4b55c
Альбина Фролова <afrolova@na.edu>
2017-06-21 23:44:12 +0600

    Added main Retail class

---
+++ Retail/Retail.java
@@ -0,0 +1,20 @@
+
+public class Retail {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Electronic var1 = new Desktop();
+        Computer var2 = new Laptop ();
+        Electronic var3 = new Computer(); 
+        Electronic var4 = new Laptop();
+        
+        var1.a();
+        var1.b();
+        var2.c();
+        var3.a();
+        var4.b();       
+    }
+    
+}
