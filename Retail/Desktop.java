37ec810f95e3ec748095ea2dfa12fbcda416284f
Альбина Фролова <afrolova@na.edu>
2017-06-21 23:42:58 +0600

    Added Desktop class

---
+++ Retail/Desktop.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author albinafrolova
+ */
+public class Desktop extends Computer{
+    public void b(){
+        System.out.println("Desktop b");
+        super.b();
+    }
+    public void c(){
+        System.out.println("Desktop c");       
+    }
+}
